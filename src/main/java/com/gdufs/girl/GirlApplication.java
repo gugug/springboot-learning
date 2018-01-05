fe90ed27276a345e1b605b9e79e03061b562ad61
gugug <gucailiang1@yy.com>
2018-01-05 12:56:36 +0800

    启动类

--- src/main/java/com/gdufs/girl/GirlApplication.java
+++ src/main/java/com/gdufs/girl/GirlApplication.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.ComponentScan;
 
 /**
  * SpringBoot中Bean注解扫描默认是扫描Application类所在的包及其子包
+ * 右键Run运行这个Application类
+ * Tomcat started on port(s): your port (http)
+ * Started Application in * seconds (JVM running for 7.398)
  */
 @SpringBootApplication
 @ComponentScan()
