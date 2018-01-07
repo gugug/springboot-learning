d9f02423ec101d537041a6ea140a33a411b32b28
gugug <734093894@qq.com>
2018-01-07 15:30:44 +0800

    jdbctemplate的多数据源配置

--- springboot-multidatasource/src/main/java/com/gdufs/multidatasource/JdbcTemplateConfig.java
+++ springboot-multidatasource/src/main/java/com/gdufs/multidatasource/JdbcTemplateConfig.java
@@ -8,6 +8,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import javax.sql.DataSource;
 
 /**
+ * 只需要为其注入对应的datasource即可，
+ * 在创建JdbcTemplate的时候分别注入名为primaryDataSource和secondaryDataSource的数据源来区分不同的JdbcTemplate。
  * Created by gu on 2018/1/7.
  */
 
