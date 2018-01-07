7b7615bc0e9c1f2a2f8d2798d6537bbd304c0608
gugug <734093894@qq.com>
2018-01-07 22:03:56 +0800

    springboot-aop日志方面实现

---
+++ springboot-aop/src/main/java/com/gdufs/aop/Application.java
@@ -0,0 +1,24 @@
+package com.gdufs.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * # AOP
+ * spring.aop.auto=true # Add @EnableAspectJAutoProxy.
+ * spring.aop.proxy-target-class=false # Whether subclass-based (CGLIB) proxies are to be created (true) as
+ * opposed to standard Java interface-based proxies (false).
+ * 而当我们需要使用CGLIB来实现AOP的时候，需要配置spring.aop.proxy-target-class=true，不然默认使用的是标准Java的实现。
+ * <p>
+ * //@EnableAspectJAutoProxy
+ * <p>
+ * Created by gu on 2018/1/7.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ springboot-aop/src/main/java/com/gdufs/aop/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.gdufs.aop.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by gu on 2018/1/7.
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello(@RequestParam String name) {
+        return "Hello " + name;
+    }
+
+}
