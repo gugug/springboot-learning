8c766055f1168ad6960c4e58fe9b35f78dbf96a2
gugug <gucailiang1@yy.com>
2018-10-10 14:36:12 +0800

    ResponseEntity中可以包含响应相关的元数据

--- springboot-restful/src/main/java/com/gdufs/rest/controller/CityRestController.java
+++ springboot-restful/src/main/java/com/gdufs/rest/controller/CityRestController.java
@@ -3,6 +3,8 @@ package com.gdufs.rest.controller;
 import com.gdufs.rest.entity.City;
 import com.gdufs.rest.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +21,25 @@ public class CityRestController {
     @RequestMapping(value = "/api/city/{id}", method = RequestMethod.GET)
     public City findOneCity(@PathVariable("id") Long id) {
         return cityService.findCityById(id);
+    }
+
+    /**
+     * ResponseEntity中可以包含响应相关的元数据（如头部信息和状态码）以及要转换成资源表述的对象。
+     * ResponseEntity允许我们指定响应的状态码，所以当无法找到Spittle的时候，我们可以返回HTTP 404错误
+     * <p>
+     * 路径中得到的ID用来从Repository中检索Spittle。如果找到的话，状态码设置
+     * 为HttpStatus.OK（这是之前的默认值），但是如果Repository返回null的话，状态码设置
+     * 为HttpStatus.NOT_FOUND，这会转换为HTTP 404。最后，会创建一个新的
+     * ResponseEntity，它会把Spittle和状态码传送给客户
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/api/city1/{id}", method = RequestMethod.GET)
+    public ResponseEntity<City> findOneCityById(@PathVariable("id") Long id) {
+        City city = cityService.findCityById(id);
+        HttpStatus status = city != null ? HttpStatus.OK : HttpStatus.NOT_FOUND;
+        return new ResponseEntity<City>(city, status);
     }
 
     @RequestMapping(value = "/api/city", method = RequestMethod.GET)
